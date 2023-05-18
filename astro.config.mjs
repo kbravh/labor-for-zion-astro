9780465c34c05cdf8ff9ba36f526c81487ff1cea
Karey Higuera <karey.higuera@gmail.com>
2023-05-17 22:37:57 -0400

    fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image()]
+  site: 'https://laborforzion.com',
+  integrations: [tailwind(), sitemap(), image()],
 });

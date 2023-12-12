7bc7401573946f79e3aadbfc3d8f8e7b643006c4
Karey Higuera <karey.higuera@gmail.com>
2023-12-12 00:05:50 -0500

    upgrade to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-import image from '@astrojs/image';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://laborforzion.com',
-  integrations: [tailwind(), sitemap(), image()],
+  integrations: [tailwind(), sitemap()],
 });

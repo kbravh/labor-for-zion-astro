a12812caab485488e29a23b46712938e5b5d5181
Karey Higuera <karey.higuera@gmail.com>
2023-12-21 10:20:18 -0500

    simplify sroll listener registration

--- src/utils/headingObserver.ts
+++ src/utils/headingObserver.ts
@@ -4,36 +4,31 @@ export const headingObserver = () => {
   const headings = document.querySelectorAll(
     '#article-body h2, #article-body h3, #article-body h4, #article-body h5, #article-body h6'
   );
+  let closestHeading: Element | null = null;
+  let closestHeadingDistance = Number.MAX_VALUE;
 
-  function updateActiveLink() {
-    let closestHeading: Element | null = null;
-    let closestHeadingDistance = Number.MAX_VALUE;
-
-    const titleBoundingRect = articleTitle?.getBoundingClientRect();
-    const titleDistance = Math.abs(titleBoundingRect?.top || Number.MAX_VALUE);
-    if (titleDistance < closestHeadingDistance && titleDistance <= 300) {
-      closestHeading = articleTitle;
-      closestHeadingDistance = titleDistance;
-    }
-
-    headings.forEach(heading => {
-      const boundingRect = heading.getBoundingClientRect();
-      const distance = Math.abs(boundingRect.top);
-      if (distance < closestHeadingDistance && boundingRect.top <= 300) {
-        closestHeading = heading;
-        closestHeadingDistance = distance;
-      }
-    });
-
-    if (closestHeading) {
-      tocLinks.forEach(link => {
-        link.classList.remove('!text-emerald-500', 'font-bold');
-        if (link.getAttribute('href') === `#${closestHeading?.id}`) {
-          link.classList.add('!text-emerald-500', 'font-bold');
-        }
-      });
-    }
+  const titleBoundingRect = articleTitle?.getBoundingClientRect();
+  const titleDistance = Math.abs(titleBoundingRect?.top || Number.MAX_VALUE);
+  if (titleDistance < closestHeadingDistance && titleDistance <= 300) {
+    closestHeading = articleTitle;
+    closestHeadingDistance = titleDistance;
   }
 
-  document.addEventListener('scroll', updateActiveLink, {passive: true});
+  headings.forEach(heading => {
+    const boundingRect = heading.getBoundingClientRect();
+    const distance = Math.abs(boundingRect.top);
+    if (distance < closestHeadingDistance && boundingRect.top <= 300) {
+      closestHeading = heading;
+      closestHeadingDistance = distance;
+    }
+  });
+
+  if (closestHeading) {
+    tocLinks.forEach(link => {
+      link.classList.remove('!text-emerald-500', 'font-bold');
+      if (link.getAttribute('href') === `#${closestHeading?.id}`) {
+        link.classList.add('!text-emerald-500', 'font-bold');
+      }
+    });
+  }
 };

f235bb867351c7ad2c995f2453120f4c881f5bd9
Karey Higuera <karey.higuera@gmail.com>
2023-12-21 09:40:15 -0500

    add scroll watcher for headings

---
+++ src/utils/headingObserver.ts
@@ -0,0 +1,39 @@
+export const headingObserver = () => {
+  const tocLinks = document.querySelectorAll('#table-of-contents a[href^="#"]');
+  const articleTitle = document.querySelector('#article-title');
+  const headings = document.querySelectorAll(
+    '#article-body h2, #article-body h3, #article-body h4, #article-body h5, #article-body h6'
+  );
+
+  function updateActiveLink() {
+    let closestHeading: Element | null = null;
+    let closestHeadingDistance = Number.MAX_VALUE;
+
+    const titleBoundingRect = articleTitle?.getBoundingClientRect();
+    const titleDistance = Math.abs(titleBoundingRect?.top || Number.MAX_VALUE);
+    if (titleDistance < closestHeadingDistance && titleDistance <= 300) {
+      closestHeading = articleTitle;
+      closestHeadingDistance = titleDistance;
+    }
+
+    headings.forEach(heading => {
+      const boundingRect = heading.getBoundingClientRect();
+      const distance = Math.abs(boundingRect.top);
+      if (distance < closestHeadingDistance && boundingRect.top <= 300) {
+        closestHeading = heading;
+        closestHeadingDistance = distance;
+      }
+    });
+
+    if (closestHeading) {
+      tocLinks.forEach(link => {
+        link.classList.remove('!text-emerald-500', 'font-bold');
+        if (link.getAttribute('href') === `#${closestHeading?.id}`) {
+          link.classList.add('!text-emerald-500', 'font-bold');
+        }
+      });
+    }
+  }
+
+  document.addEventListener('scroll', updateActiveLink, {passive: true});
+};

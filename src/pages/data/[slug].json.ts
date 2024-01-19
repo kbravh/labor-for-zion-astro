bc9c5d78284325441883a4f6d18972111ce36105
Karey Higuera <karey.higuera@gmail.com>
2024-01-19 10:29:20 -0500

    use custom route for page views instead of form handling

---
+++ src/pages/data/[slug].json.ts
@@ -0,0 +1,39 @@
+import type {APIRoute} from 'astro';
+import {logPageView} from '../../utils/db';
+import { getTitleAndSlugMaps } from '../../utils/mdUtils';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({params}) => {
+  const {slug} = params;
+
+  if (!slug) {
+    return new Response(null, {
+      status: 400,
+      statusText: 'No slug'
+    });
+  }
+
+  const {slugToTitle} = getTitleAndSlugMaps();
+  const set = new Set([...Object.keys(slugToTitle)])
+
+  if (!set.has(slug)) {
+    return new Response(null, {
+      status: 404,
+      statusText: 'Slug not found'
+    })
+  }
+
+  const count = await logPageView(slug);
+  return new Response(
+    JSON.stringify({
+      count,
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+};

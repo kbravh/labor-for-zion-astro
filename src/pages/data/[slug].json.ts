57001731434b40ba341777e2c00dba6efed4fd67
Karey Higuera <karey.higuera@gmail.com>
2024-01-19 10:34:17 -0500

    don't log dev visits

--- src/pages/data/[slug].json.ts
+++ src/pages/data/[slug].json.ts
@@ -1,27 +1,36 @@
 import type {APIRoute} from 'astro';
 import {logPageView} from '../../utils/db';
-import { getTitleAndSlugMaps } from '../../utils/mdUtils';
+import {getTitleAndSlugMaps} from '../../utils/mdUtils';
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({params}) => {
+  if (import.meta.env.DEV) {
+    return new Response(JSON.stringify({count: 1123}), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   const {slug} = params;
 
   if (!slug) {
     return new Response(null, {
       status: 400,
-      statusText: 'No slug'
+      statusText: 'No slug',
     });
   }
 
   const {slugToTitle} = getTitleAndSlugMaps();
-  const set = new Set([...Object.keys(slugToTitle)])
+  const set = new Set([...Object.keys(slugToTitle)]);
 
   if (!set.has(slug)) {
     return new Response(null, {
       status: 404,
-      statusText: 'Slug not found'
-    })
+      statusText: 'Slug not found',
+    });
   }
 
   const count = await logPageView(slug);

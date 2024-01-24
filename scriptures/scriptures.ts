064e1d1e7b0a1fe8dc6447daffd5633e74141306
Karey Higuera <karey.higuera@gmail.com>
2024-01-23 23:04:42 -0500

    add scriptures from scraper

---
+++ scriptures/scriptures.ts
@@ -0,0 +1,21 @@
+export type Work = {
+  link: string,
+  name: string
+  books: Book[]
+}
+export type Book = {
+  name: string
+  summary?: string
+  chapters: Chapter[]
+}
+
+export type Chapter = {
+  number?: number
+  summary?: string
+  verses: Verse[]
+}
+
+export type Verse = {
+  number: number
+  text: string
+}

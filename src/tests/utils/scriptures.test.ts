6e8d34094ccef84093c0e04ccd091a152e9415bd
Karey Higuera <karey.higuera@gmail.com>
2024-01-24 20:01:37 -0500

    switch to vite and add scripture identify function

--- src/tests/utils/scriptures.test.ts
+++ src/tests/utils/scriptures.test.ts
@@ -1,19 +1,23 @@
 import {isScriptureReference} from '../../utils/scriptures';
+import {describe, expect, it} from 'vitest';
 
 describe('isScriptureReference', () => {
   it('ignores regular title', () => {
     expect(isScriptureReference('And My Father Dwelt in a Tent')).toBeFalsy();
   });
   it('ignores title with numbers', () => {
-    expect(isScriptureReference('And My Father Dwelt in a Tent')).toBeFalsy();
+    expect(isScriptureReference('The 4 Gospels')).toBeFalsy();
   });
   it('recognizes a single verse', () => {
-    expect(isScriptureReference('And My Father Dwelt in a Tent')).toBeTruthy();
+    expect(isScriptureReference('Book of Mormon 25.26')).toBeTruthy();
+  });
+  it('recognizes a single verse with a colon', () => {
+    expect(isScriptureReference('Book of Mormon 25:26')).toBeTruthy();
   });
   it('recognizes a verse range', () => {
-    expect(isScriptureReference('And My Father Dwelt in a Tent')).toBeTruthy();
+    expect(isScriptureReference('Doctrine & Covenants 111.12-14')).toBeTruthy();
   });
   it('recognizes multiple verse ranges', () => {
-    expect(isScriptureReference('And My Father Dwelt in a Tent')).toBeTruthy();
+    expect(isScriptureReference('Joseph Smith-History 1.2-4,7-9-12')).toBeTruthy();
   });
 });

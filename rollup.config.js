ac074a3f9b2d1d5a0b66a61d0323ed4fcd0453b5
Karey Higuera <karey.higuera@gmail.com>
2024-06-11 15:58:29 -0400

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'functions/log-view/index.ts',
@@ -8,5 +9,5 @@ export default {
     file: 'functions/log-view/dist/bundle.js',
     format: 'cjs',
   },
-  plugins: [resolve(), commonjs(), typescript()],
+  plugins: [resolve(), commonjs(), json(), typescript()],
 };

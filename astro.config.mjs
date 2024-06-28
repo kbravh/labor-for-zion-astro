c8b91b3b91b798d376de836f38009daf1cc7435f
Karey Higuera <karey.higuera@gmail.com>
2024-06-27 23:25:32 -0400

    remove views from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://laborforzion.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap({
+    filter: (page) => page !== 'https://laborforzion.com/views',
+  })],
 });

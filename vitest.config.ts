6106cec532cee7548f5c479783efb8fe521d7737
Karey Higuera <karey.higuera@gmail.com>
2024-07-22 22:37:24 -0400

    add test coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,12 @@
+/// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
 
-export default getViteConfig({});
+export default getViteConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**'],
+    },
+  }
+});

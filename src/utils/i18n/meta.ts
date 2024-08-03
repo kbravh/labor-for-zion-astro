55f4094b2d0d47843dd10087dc4bab8efb8b91ca
Karey Higuera <karey.higuera@gmail.com>
2024-08-02 21:05:51 -0400

    add footer i18n

--- src/utils/i18n/meta.ts
+++ src/utils/i18n/meta.ts
@@ -1,20 +1,33 @@
-import type { Locale } from "@validation/i18n";
+import type {Locale} from '@validation/i18n';
 
-export type Meta = 'site' | 'description';
+export type Meta =
+  | 'site'
+  | 'description'
+  | 'themeReference'
+  | 'theme1'
+  | 'theme2';
 
 export type MetaMap = {
   [k in Locale]: {
-    [k in Meta]: string
-  }
-}
+    [k in Meta]: string;
+  };
+};
 
 export const metaMap: MetaMap = {
   en: {
     site: 'Labor For Zion',
-    description: 'A collection of notes, talks, and tools centered around gospel topics.'
+    description:
+      'A collection of notes, talks, and tools centered around gospel topics.',
+    theme1: `"But the laborer in Zion shall labor for Zion;`,
+    theme2: `for if they labor for money they shall perish."`,
+    themeReference: '2 Nephi 26:31',
   },
   es: {
     site: 'Obrero en Sión',
-    description: 'Una colección de notas, discursos, y herramientas centrados en el evangelio.'
+    description:
+      'Una colección de notas, discursos, y herramientas centrados en el evangelio.',
+    theme1: `"Mas el obrero en Sion trabajará para Sion;`,
+    theme2: `porque si trabaja por dinero, perecerá."`,
+    themeReference: '2 Nefi 26:31',
   },
-}
+};

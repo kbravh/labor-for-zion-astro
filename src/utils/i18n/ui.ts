13ba7fbd4c8d507417e26972b52cb80da5ffddf5
Karey Higuera <karey.higuera@gmail.com>
2024-08-02 21:19:41 -0400

    add i18n to pagination

--- src/utils/i18n/ui.ts
+++ src/utils/i18n/ui.ts
@@ -1,6 +1,6 @@
-import type {Locale} from '@validation/i18n';
+import type { Locale } from "@validation/i18n";
 
-export type UI = 'menu' | 'backlinks';
+export type UI = "menu" | "backlinks" | "page" | "next" | "previous";
 
 export type UIStrings = {
   [k in Locale]: {
@@ -10,11 +10,17 @@ export type UIStrings = {
 
 export const uiStrings: UIStrings = {
   en: {
-    menu: 'Menu',
-    backlinks: 'Pages that reference this note:'
+    menu: "Menu",
+    backlinks: "Pages that reference this note:",
+    next: 'Next page',
+    page: 'Page',
+    previous: 'Previous page'
   },
   es: {
-    menu: 'Menú',
-    backlinks: 'Páginas que mencionan esta nota:'
+    menu: "Menú",
+    backlinks: "Páginas que mencionan esta nota:",
+    next: 'Siguiente',
+    page: 'Página',
+    previous: 'Anterior'
   },
 } as const;

426c6d3a6522d5282186a29807668b8ce216b7f7
Karey Higuera <karey.higuera@gmail.com>
2024-09-30 07:34:52 -0400

    update hrefs to be relative to locale

--- src/utils/i18n/ui.ts
+++ src/utils/i18n/ui.ts
@@ -5,6 +5,7 @@ export type UI =
   | "empty"
   | "menu"
   | "next"
+  | "note_about"
   | "notes_about"
   | "page"
   | "previous"
@@ -26,6 +27,7 @@ export const uiMap: UIMap = {
       "This page hasn't been created yet! But if it has been mentioned somewhere else, you can see those references below.",
     menu: "Menu",
     next: "Next page",
+    note_about: "A note about",
     notes_about: "Notes about: ",
     page: "Page",
     previous: "Previous page",
@@ -40,6 +42,7 @@ export const uiMap: UIMap = {
       "Esta página aún no se ha creado! Pero si ha sido mencionado en otro lado, podrás ver esas referencias aquí.",
     menu: "Menú",
     next: "Siguiente",
+    note_about: "Una nota sobre",
     notes_about: "Notas sobre: ",
     page: "Página",
     previous: "Anterior",

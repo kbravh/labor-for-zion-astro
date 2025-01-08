ded8c69286cd3b1e4ac50d31a38c92842e657700
Karey Higuera <karey.higuera@gmail.com>
2025-01-08 16:40:27 -0500

    Change analytics to SQL connection and track UTM tags (#5)
    * add schema and analytics
    
    * add astro actions for page views
    
    * remove lambda functions and add sanity checks to action
    
    * capture screen size and referrer

---
+++ src/actions/index.ts
@@ -0,0 +1,54 @@
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { PrismaClient } from "@prisma/client";
+import { isValidLocale } from "@validation/i18n";
+import { getTitleAndSlugMaps } from "@utils/md/readAndParse";
+
+const prisma = new PrismaClient();
+
+export const server = {
+  logPageView: defineAction({
+    input: z.object({
+      slug: z.string(),
+      locale: z.string(),
+      utm_source: z.string().optional(),
+      utm_medium: z.string().optional(),
+      utm_campaign: z.string().optional(),
+      utm_content: z.string().optional(),
+      utm_term: z.string().optional(),
+    }),
+    handler: async (data) => {
+      if (!isValidLocale(data.locale)) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: "Not a valid locale",
+        });
+      }
+
+      const { slugToTitle } = await getTitleAndSlugMaps(data.locale);
+      const set = new Set(Object.keys(slugToTitle));
+
+      if (!set.has(data.slug)) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: "Slug not found",
+        });
+      }
+
+      return prisma.analytics.create({ data });
+    },
+  }),
+  getPageViews: defineAction({
+    input: z.object({
+      slug: z.string(),
+      locale: z.string(),
+    }),
+    handler: async ({ slug, locale }) =>
+      prisma.analytics.count({
+        where: {
+          slug,
+          locale,
+        },
+      }),
+  }),
+};

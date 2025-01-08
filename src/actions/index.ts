ec5bcd5b5c885f4c599512e87c126ef0ff75ff05
Karey Higuera <karey.higuera@gmail.com>
2025-01-08 16:47:31 -0500

    add referrer and resolution to action validation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -16,6 +16,8 @@ export const server = {
       utm_campaign: z.string().optional(),
       utm_content: z.string().optional(),
       utm_term: z.string().optional(),
+      referrer: z.string().optional(),
+      screenResolution: z.string().optional(),
     }),
     handler: async (data) => {
       if (!isValidLocale(data.locale)) {
